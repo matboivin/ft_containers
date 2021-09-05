7e9c62995646f829016ffaf5e9c4d343819d5473
ouram <mboivin@student.42.fr>
2021-09-05 17:01:08 +0200

    Add prototypes for iterators and capacity

--- include/vector.hpp
+++ include/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 15:25:08 by mboivin           #+#    #+#             */
-/*   Updated: 2021/09/05 16:49:29 by mboivin          ###   ########.fr       */
+/*   Updated: 2021/09/05 17:00:28 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,37 @@ namespace ft {
 		// typedef std::iterator_traits<iterator>::difference_type	difference_type;
 		// TODO: Re-implement iterators_traits and reverse_iterator
 		typedef std::size_t										size_type;
+
+		// Default constructor
+		Vector( void ) : Vector( Allocator() ) {}
+
+		// Destructor
+		~Vector( void ) {}
+
+		// Assignment operator
+		Vector&	operator=( const Vector& rhs ) {
+
+			static_cast<void>(rhs);
+			return (*this); // tmp
+		}
+
+		// iterators
+		// iterator				begin( void );
+		// iterator				end( void );
+		// reverse_iterator		rbegin( void );
+		// reverse_iterator		rend( void );
+		// const_iterator			cbegin( void );
+		// const_iterator			cend( void );
+		// const_reverse_iterator	crbegin( void );
+		// const_reverse_iterator	crend( void );
+
+		// capacity
+		bool		empty( void ) const;
+		size_type	size( void ) const;
+		size_type	max_size( void ) const;
+		size_type	capacity( void ) const;
+		void		reserve( size_type n );
+		void		shrink_to_fit( void );
 
 	};
 
