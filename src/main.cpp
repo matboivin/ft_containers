6e36484333c1df1656cbfa9f244e6cb822184266
ouram <mboivin@student.42.fr>
2021-09-06 17:17:49 +0200

    Add naive tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,35 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 15:23:19 by mboivin           #+#    #+#             */
-/*   Updated: 2021/09/06 16:12:33 by mboivin          ###   ########.fr       */
+/*   Updated: 2021/09/06 17:17:26 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include <vector.hpp>
 #include <vector>
+
+template< typename Vec >
+static void	displayInfos( const std::string& title, const Vec& v ) {
+
+	std::cout << title
+			  << "\n- size:        " << v.size()
+			  << "\n- capacity:    " << v.capacity() << std::endl;
+
+	for ( std::size_t i = 0; i < v.size(); i++ )
+		std::cout << v[i] << ' ';
+
+	std::cout << "\n\n";
+}
 
 void	testDefaultConstructors( void ) {
 
 	ft::vector<int>		ft_vec;
 	std::vector<int>	vec;
 
-	std::cout << "\n\nft_vec size:     " << ft_vec.size()
-			  << "\nft_vec capacity: " << ft_vec.capacity()
-			  << "\nft_vec max_size: " << ft_vec.max_size() << std::endl;
-
-	std::cout << "\nvec size:        " << vec.size()
-			  << "\nvec capacity:    " << vec.capacity()
-			  << "\nvec max_size:    " << vec.max_size() << std::endl;
+	displayInfos("ft_vec", ft_vec);
+	displayInfos("vec", vec);
 }
 
 void	testConstructors( void ) {
@@ -34,61 +43,79 @@ void	testConstructors( void ) {
 	std::vector<int>	vec2(42, 2);
 	std::vector<int>	vec3(4,  3);
 
-	std::cout << "VEC 1"
-			  << "\nvec size:        " << vec1.size()
-			  << "\nvec capacity:    " << vec1.capacity() << std::endl;
-
-	std::cout << "\nVEC 2"
-			  << "\nvec size:        " << vec2.size()
-			  << "\nvec capacity:    " << vec2.capacity() << std::endl;
-
-	std::cout << "\nVEC 3"
-			  << "\nvec size:        " << vec3.size()
-			  << "\nvec capacity:    " << vec3.capacity() << std::endl;
+	displayInfos("vec 1", vec1);
+	displayInfos("vec 2", vec2);
+	displayInfos("vec 3", vec3);
 
 	vec2 = vec1;
 	vec3 = vec1;
 
-	std::cout << "\n\nVEC 2"
-			  << "\nvec size:        " << vec2.size()
-			  << "\nvec capacity:    " << vec2.capacity() << std::endl;
+	std::cout << "AFTER\n\n";
 
-	std::cout << "\nVEC 3"
-			  << "\nvec size:        " << vec3.size()
-			  << "\nvec capacity:    " << vec3.capacity() << std::endl;
+	displayInfos("vec 2", vec2);
+	displayInfos("vec 3", vec3);
+
+	std::cout << "______________\n\n";
 
 	std::vector<int>	ft_vec1(14, 1);
 	std::vector<int>	ft_vec2(42, 2);
 	std::vector<int>	ft_vec3(4,  3);
 
-		std::cout << "\n\nFT_VEC 1"
-			  << "\nvec size:        " << ft_vec1.size()
-			  << "\nvec capacity:    " << ft_vec1.capacity() << std::endl;
-
-	std::cout << "\nFT_VEC 2"
-			  << "\nvec size:        " << ft_vec2.size()
-			  << "\nvec capacity:    " << ft_vec2.capacity() << std::endl;
-
-	std::cout << "\nFT_VEC 3"
-			  << "\nvec size:        " << ft_vec3.size()
-			  << "\nvec capacity:    " << ft_vec3.capacity() << std::endl;
+	displayInfos("ft_vec 1", ft_vec1);
+	displayInfos("ft_vec 2", ft_vec2);
+	displayInfos("ft_vec 3", ft_vec3);
 
 	ft_vec2 = ft_vec1;
 	ft_vec3 = ft_vec1;
 
-	std::cout << "\n\nFT_VEC 2"
-			  << "\nvec size:        " << ft_vec2.size()
-			  << "\nvec capacity:    " << ft_vec2.capacity() << std::endl;
+	std::cout << "AFTER\n\n";
 
-	std::cout << "\nFT_VEC 3"
-			  << "\nvec size:        " << ft_vec3.size()
-			  << "\nvec capacity:    " << ft_vec3.capacity() << std::endl;
+	displayInfos("ft_vec 2", ft_vec2);
+	displayInfos("ft_vec 3", ft_vec3);
+}
+
+void	testCopy( void ) {
+
+	std::vector<int>	vec1(14);
+	std::vector<int>	vec2(4);
+
+	vec1.push_back(3);
+	vec1.push_back(5);
+	vec1.push_back(737);
+	vec1.push_back(1);
+	vec1.push_back(5);
+	vec1.push_back(58);
+	vec1.push_back(0);
+
+	displayInfos("vec 1", vec1);
+	displayInfos("vec 2", vec2);
+
+	vec2 = vec1;
+
+	std::cout << "AFTER\n\n";
+
+	displayInfos("vec 2", vec2);
+
+	std::cout << "______________\n\n";
+
+	std::vector<int>	ft_vec1(14, 1);
+	std::vector<int>	ft_vec2(4,  3);
+
+	displayInfos("ft_vec 1", ft_vec1);
+	displayInfos("ft_vec 2", ft_vec2);
+
+	ft_vec2 = ft_vec1;
+
+	std::cout << "AFTER\n\n";
+
+	displayInfos("ft_vec 2", ft_vec2);
 }
 
 int	main( void ) {
 
 	//testDefaultConstructors();
-	testConstructors();
+	//testConstructors();
+	testCopy();
 
 	return (0);
 }
