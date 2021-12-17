db9df4dcf733a466445ec88db81d4efc84e3e20a
Mathilde BOIVIN <mboivin@student.42.fr>
2021-12-18 00:38:46 +0100

    Add tests

--- tests/src/map/tree_tests.cpp
+++ tests/src/map/tree_tests.cpp
@@ -265,6 +265,87 @@ namespace ft
 		std::cout << "tree max size:  " << tree.max_size() << std::endl;
 	}
 
+	void	test_tree_count(void)
+	{
+		std::cout << "TEST: Element access: count() \n\n";
+
+		explainTest("Returns 1 if the tree contains an element matching key k.");
+
+		std::map<int, std::string>	m;
+		tree_type					tree;
+
+		m[8]  = "eight";
+		m[18] = "eighteen";
+		m[5]  = "five";
+		m[15] = "fifteen";
+		m[17] = "seventeen";
+		m[25] = "twenty-five";
+		m[40] = "fourty";
+		m[80] = "eigthy";
+		m[11] = "eleven";
+		m[20] = "twenty";
+
+		tree.insert( pair<int, std::string>(8,  "eight") );
+		tree.insert( pair<int, std::string>(18, "eighteen") );
+		tree.insert( pair<int, std::string>(5,  "five") );
+		tree.insert( pair<int, std::string>(15, "fifteen") );
+		tree.insert( pair<int, std::string>(17, "seventeen") );
+		tree.insert( pair<int, std::string>(25, "twenty-five") );
+		tree.insert( pair<int, std::string>(40, "fourty") );
+		tree.insert( pair<int, std::string>(80, "eighty") );
+		tree.insert( pair<int, std::string>(11, "eleven") );
+		tree.insert( pair<int, std::string>(20, "twenty") );
+
+		std::cout << "m.count(5):                     " << m.count(5) << '\n'
+				  << "tree.count(5):                  " << tree.count(5) << '\n'
+				  << "m.count(40):                    " << m.count(40) << '\n'
+				  << "tree.count(40):                 " << tree.count(40) << "\n\n"
+				  << "m.count(99):                    " << m.count(99) << '\n'
+				  << "tree.count(99):                 " << tree.count(99)
+				  << "\n\n";
+	}
+
+	void	test_tree_find(void)
+	{
+		std::cout << "TEST: Element access: find() \n\n";
+
+		explainTest("Returns the element with the key k, else return end.");
+
+		std::map<int, std::string>	m;
+		tree_type					tree;
+
+		m[8]  = "eight";
+		m[18] = "eighteen";
+		m[5]  = "five";
+		m[15] = "fifteen";
+		m[17] = "seventeen";
+		m[25] = "twenty-five";
+		m[40] = "fourty";
+		m[80] = "eigthy";
+		m[11] = "eleven";
+		m[20] = "twenty";
+
+		tree.insert( pair<int, std::string>(8,  "eight") );
+		tree.insert( pair<int, std::string>(18, "eighteen") );
+		tree.insert( pair<int, std::string>(5,  "five") );
+		tree.insert( pair<int, std::string>(15, "fifteen") );
+		tree.insert( pair<int, std::string>(17, "seventeen") );
+		tree.insert( pair<int, std::string>(25, "twenty-five") );
+		tree.insert( pair<int, std::string>(40, "fourty") );
+		tree.insert( pair<int, std::string>(80, "eighty") );
+		tree.insert( pair<int, std::string>(11, "eleven") );
+		tree.insert( pair<int, std::string>(20, "twenty") );
+
+		std::cout << "m.find(5):                     " << m.find(5)->first << '\n'
+				  << "tree.find(5):                  " << tree.find(5)->first << '\n'
+				  << "m.find(40):                    " << m.find(40)->first << '\n'
+				  << "tree.find(40):                 " << tree.find(40)->first << "\n\n"
+				  << "(m.find(99) == m.end())?       " << (m.find(99) == m.end()) << '\n'
+				  << "(tree.find(99) == tree.end())? " << (tree.find(99) == tree.end())
+				  << "\n\n";
+	}
+
+
 	void	test_tree_lower_bound(void)
 	{
 		std::cout << "TEST: Element access: lower_bound() \n\n";
@@ -342,46 +423,6 @@ namespace ft
 				  << "tree.upper_bound(24):                 " << tree.upper_bound(24)->first << "\n\n"
 				  << "(m.upper_bound(99) == m.end())?       " << (m.upper_bound(99) == m.end()) << '\n'
 				  << "(tree.upper_bound(99) == tree.end())? " << (tree.upper_bound(99) == tree.end())
-				  << "\n\n";
-	}
-
-	void	test_tree_find(void)
-	{
-		std::cout << "TEST: Element access: find() \n\n";
-
-		explainTest("Returns the element with the key k, else return end.");
-
-		std::map<int, std::string>	m;
-		tree_type					tree;
-
-		m[8]  = "eight";
-		m[18] = "eighteen";
-		m[5]  = "five";
-		m[15] = "fifteen";
-		m[17] = "seventeen";
-		m[25] = "twenty-five";
-		m[40] = "fourty";
-		m[80] = "eigthy";
-		m[11] = "eleven";
-		m[20] = "twenty";
-
-		tree.insert( pair<int, std::string>(8,  "eight") );
-		tree.insert( pair<int, std::string>(18, "eighteen") );
-		tree.insert( pair<int, std::string>(5,  "five") );
-		tree.insert( pair<int, std::string>(15, "fifteen") );
-		tree.insert( pair<int, std::string>(17, "seventeen") );
-		tree.insert( pair<int, std::string>(25, "twenty-five") );
-		tree.insert( pair<int, std::string>(40, "fourty") );
-		tree.insert( pair<int, std::string>(80, "eighty") );
-		tree.insert( pair<int, std::string>(11, "eleven") );
-		tree.insert( pair<int, std::string>(20, "twenty") );
-
-		std::cout << "m.find(5):                     " << m.find(5)->first << '\n'
-				  << "tree.find(5):                  " << tree.find(5)->first << '\n'
-				  << "m.find(40):                    " << m.find(40)->first << '\n'
-				  << "tree.find(40):                 " << tree.find(40)->first << "\n\n"
-				  << "(m.find(99) == m.end())?       " << (m.find(99) == m.end()) << '\n'
-				  << "(tree.find(99) == tree.end())? " << (tree.find(99) == tree.end())
 				  << "\n\n";
 	}
 
@@ -517,9 +558,10 @@ namespace ft
 		ft::test_tree_max_size();
 
 		// elements access
+		ft::test_tree_count();
+		ft::test_tree_find();
 		ft::test_tree_lower_bound();
 		ft::test_tree_upper_bound();
-		ft::test_tree_find();
 
 		// modifiers
 		ft::test_tree_insert();
